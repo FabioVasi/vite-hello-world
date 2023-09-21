b9fb4f2bdd34eca20a62b00f71de33051ad48e23
FabioVasi <vasifabio96@gmail.com>
2023-09-21 16:55:17 +0200

    add first part of the bonus

--- vite-hello-world/src/App.vue
+++ vite-hello-world/src/App.vue
@@ -1,17 +1,21 @@
 <script>
 
   import HelloWorld from "./components/HelloWorld.vue";
+  import AppTitle from "./components/AppTitle.vue";
   export default {
     components: {
-      HelloWorld
+      HelloWorld,
+      AppTitle
     }
   }
+  
 
 </script>
 
 <template>
  
   <HelloWorld/>
+  <AppTitle/>
 
 </template>
 
---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <h2 class="title">La mia prima app con Vite</h2>
+
+</template>
+
+<style>
+
+    .title{
+        color: greenyellow;
+        font-size: 32px;
+    }
+    .title:hover{
+        color: blueviolet;
+    }
+
+</style>
